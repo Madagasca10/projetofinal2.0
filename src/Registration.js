a13426012878c7260a4855d0bfd2663dd23ddc01
Madagasca10 <lopesjoao775@gmail.com>
2021-02-03 15:27:15 +0000

    Criação da API pelo heroku e do css
    ainda inconpletos e não funcionais.

--- src/Registration.js
+++ src/Registration.js
@@ -1,8 +1,42 @@
-import React from 'react';
-import "./form.css";
+import React, {useState,useEffect} from 'react';
+import {useHistory} from "react-router-dom";
+
+/* import "./form.css"; */
 /* import "./App.css"; */
 
+
 function Registration(){
+
+const[Nome,setNome]=useState("");
+const[password,setpassword]=useState("");
+
+
+
+const history = useHistory();
+useEffect(() =>{
+    if(localStorage.getItem("user-info")){
+        history.push("./login")
+    }
+},[])
+
+
+async function Register(){
+
+console.warn(Nome,password)
+let item={Nome,password};
+let result = await fetch("https://apipw2.herokuapp.com/Utilizadores",{
+    method:'POST',
+    headers:{
+            "Content-Type":"application/json",
+            "Accept":"application/json"
+        },
+    body:JSON.stringify(item)
+    });
+    result = await result.json();
+    localStorage.setItem("user-info",JSON.stringify(result))
+    history.push("./login")
+
+}
     return(
         
         <div className="Registration">
@@ -10,17 +44,19 @@ function Registration(){
             <h1>Registration</h1>
 
             <form>
-                <label>
-                    Username: 
-                <input type="text" name="name" />
-                </label>
-                <p></p>
-                <label>
-                    Password: 
-                <input type="text" name="password" />
-                </label>
-                <p></p>
-                <input type="submit" value="Submit" />
+                <div>
+                Username: 
+                    <input type="text" placeholder="nome"
+                     onChange={(e) => setNome(e.target.value)} className="nome"></input>
+                </div>
+                <br></br>
+                <div>
+                Password: 
+                    <input type="text" placeholder="password"
+                    onChange={(e) => setpassword(e.target.value)} className="password"></input>
+                </div>
+                <br></br>
+                <button onClick={Register} className="botão">Sign Up</button>
             </form>
 
         </div>
@@ -30,3 +66,4 @@ function Registration(){
 }   
 
 export default Registration;
+
